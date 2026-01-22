54086b0d3b76e8d0ed1d5d5ba302422792e50c83
Neo1917 <jhongomez191219@gmail.com>
2026-01-22 04:30:46 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -4,8 +4,21 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+  
+  let Who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let When = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  function generarExcusa(){
+  let randomWho = Who[Math.floor(Math.random()* Who.length)]
+  let randomAction = action[Math.floor(Math.random()* action.length)]
+  let randomWhat = what[Math.floor(Math.random()* what.length)]
+  let randomWhen = When[Math.floor(Math.random()* When.length)]
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  window.onload = function() {
+    const excusa = document.getElementById('excuse');
+      excusa.innerHTML = generarExcusa()
+        console.log("Hello Rigo from the console!");
+        };
